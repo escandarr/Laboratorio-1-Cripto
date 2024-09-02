8c248db7215bb91783cdb7e4452a675e0340cbc0
Benjamin Escandar <114037139+escandarr@users.noreply.github.com>
2024-09-02 18:02:07 -0400

    Update cesar.py

--- Codes/cesar.py
+++ Codes/cesar.py
@@ -1,1 +1,24 @@
+def cifrado_cesar(mensaje, desplazamiento):
+    resultado = ""
+    for char in mensaje:
+        if char.isalpha():  
+            base = ord('A') if char.isupper() else ord('a')
+            desplazado = (ord(char) - base + desplazamiento) % 26
+            resultado += chr(base + desplazado)
+        else:
+            resultado += char
+    return resultado
 
+def descifrar_cesar(mensaje, desplazamiento):
+    return cifrado_cesar ((mensaje, -desplazamiento)
+
+def main():
+    mensaje = input("Ingrese el mensaje: ")
+    desplazamiento = int(input("Ingrese el Rot: "))
+    
+    mensaje_cifrado = cifrado_cesar ((mensaje, desplazamiento)
+    print(f"Mensaje cifrado: {mensaje_cifrado}")
+    
+
+if __name__ == "__main__":
+    main()
